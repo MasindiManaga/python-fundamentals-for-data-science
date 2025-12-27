286e07e7b6978bc9a182f53af79edf4973e9cd09
Masindi <managamasindi@gmail.com>
2025-12-27 12:29:00 +0200

    Add OOP basics script one and two

---
+++ oop_basics/01_classes_and_objects.py
@@ -0,0 +1,13 @@
+# A class is a blueprint
+# It describes how something should look and behave
+
+class Person:
+    pass
+
+# Creating an Object/ Instantiation
+person1 = Person()
+person2 = Person()
+
+# Person = blueprint
+# person1, person2 = real objects
+# A class is an object
